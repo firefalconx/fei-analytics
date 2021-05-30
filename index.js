ad77cca7fcf4749d1b41b0880eb474d49d8430d8
Fire Falcon <xxxfirefalconxxx@gmail.com>
2021-05-29 23:48:31 -0400

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var app = require('./app');
 
 //Set app to run on port #3000
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3050);
 
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
